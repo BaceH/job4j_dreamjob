ffcace78a4f16318e032e006b9dd086bcb878c55
VasylB <vasiliy.byalyk@dstarlab.com>
2021-04-22 13:08:40 +0300

    1. Написать тест на PostServlet. [#453991] add test doGet

--- src/test/java/ru/job4j/dream/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -7,19 +7,23 @@ import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import ru.job4j.dream.model.User;
 import ru.job4j.dream.store.MemStore;
 import ru.job4j.dream.store.PsqlStore;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(PsqlStore.class)
@@ -40,5 +44,33 @@ public class PostServletTest {
 
         new PostServlet().doPost(req, resp);
         assertThat(memStore.findByIdPost(4).getName(), is("New Java work!!!"));
+    }
+
+    @Test
+    public void doGetTestResponse() throws IOException, ServletException {
+        PostServlet postServlet = new PostServlet();
+
+        MemStore memStore = MemStore.instOf();
+        PowerMockito.mockStatic(PsqlStore.class);
+        Mockito.when(PsqlStore.instOf()).thenReturn(memStore);
+
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpSession session = mock(HttpSession.class);
+        session.setAttribute("user", new User(0, "test", "email@local", "test"));
+        when(request.getSession()).thenReturn(session);
+
+        ServletContext context = mock(ServletContext.class);
+        when(request.getServletContext()).thenReturn(context);
+
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        when(request.getRequestDispatcher("posts.jsp")).thenReturn(dispatcher);
+
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        postServlet.doGet(request, response);
+
+        verify(request).getRequestDispatcher("posts.jsp");
+        verify(request).getSession();
+        verify(dispatcher).forward(request, response);
     }
 }
