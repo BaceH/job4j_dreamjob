dba5ff43a4ac26ee139c3a5e1b565806b8eabf66
VasylB <vasiliy.byalyk@dstarlab.com>
2021-04-26 12:24:53 +0300

    4.1. JSON формат. [#453954]

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,9 +1,13 @@
 package ru.job4j.dream.servlet;
 
+import netscape.javascript.JSObject;
+import org.json.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -18,6 +22,33 @@ public class GreetingServlet extends HttpServlet {
         System.out.println("Nice to meet you, " + email);
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
         writer.println("Nice to meet you, " + email);
+        writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = req.getReader();
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } finally {
+            reader.close();
+        }
+        JSONObject reqJson = new JSONObject(sb.toString());
+        System.out.println(sb.toString());
+        String resStr = "Nice to meet you, " + reqJson.getString("text");
+
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        JSONObject resJson = new JSONObject();
+        resJson.put("textRes", resStr);
+        writer.println(resJson.toString());
         writer.flush();
     }
 }
